6dabc8fec70bca08a78ecc8708a53f39fdb2e5e3
Nila <nilavosoughzadeh@gmail.com>
2025-03-17 15:46:41 +0330

    update

--- djproject/myapp/models.py
+++ djproject/myapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Messages(models.Model):
     Name = models.CharField(null=False , max_length=100)
-    Email = models.EmailField(null=False , max_length=150)
+    Email = models.EmailField(null=True , max_length=150)
     Phone = models.PositiveIntegerField(null=True)
-    Title = models.CharField(null=False , max_length=100)
+    Title = models.CharField(null=True , max_length=100)
     Text = models.TextField(null=False)
